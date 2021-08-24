2556dabe260e3503c5dd8c0b7c8159c37846795e
April Polubiec <aprilpolubiec@gmail.com>
2021-08-24 12:14:58 -0500

    start general programming & systems design solutions (not complete)

---
+++ general-programming/rectangular-love.js
@@ -0,0 +1,29 @@
+/**
+ * Write a function to find the rectangular intersection of two
+ * given love rectangles. As with the example above, love rectangles are
+ * always "straight" and never "diagonal." More rigorously: each side is
+ * parallel with either the x-axis or the y-axis. */
+
+const rectangle1 = {
+  // Coordinates of bottom-left corner
+  leftX: 1,
+  bottomY: 1,
+
+  // Width and height
+  width: 6,
+  height: 3,
+};
+
+const rectangle2 = {
+  // Coordinates of bottom-left corner
+  leftX: 4,
+  bottomY: 1,
+
+  // Width and height
+  width: 3,
+  height: 7,
+};
+
+const findIntersection = (rect1, rect2) => {
+    // First find the intersection points
+}
